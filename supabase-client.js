8309366ec346bbf7e154819424f3bc8faccb07c7
Yash Jain <06jainyash@gmail.com>
2026-05-16 11:15:26 +0530

    fix: add supabase-client.js to repository

---
+++ supabase-client.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://yiknrcgqpdlyklskpveg.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlpa25yY2dxcGRseWtsc2twdmVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NzQxOTEsImV4cCI6MjA5NDQ1MDE5MX0.EKsDfV6BG45X0Ni5T4EAzVpwv9jcEdeXbN0d_ZBrzl4';
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = supabaseClient;
